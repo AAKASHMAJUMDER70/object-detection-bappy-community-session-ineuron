b1fd5862774b13534a7a5eff7ae6a9553d12e7ed
Aakash Majumder <aakashmajumder70@gmail.com>
2024-03-14 16:10:17 +0530

    folder structure updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'aakash majumder',
+    author_email= 'aakashmajumder70@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
